0213b860e034f521dbc74926304b542e808081dc
Alex MAINGUY <alex.mainguy@etu.univ-nantes.fr>
2021-03-27 10:56:41 +0100

    Ajout résolution approchée

--- exo4.jl
+++ exo4.jl
@@ -2,15 +2,95 @@
 #creationDistancier("/Users/thomaslapierre/Desktop/Licence Informatique/Semestre 6/Recherche opérationnelle/Projet/OptimisationTourneeDrones/A/VRPA10.dat")
 #creationDistancier("/Users/thomaslapierre/Desktop/Licence Informatique/Semestre 6/Recherche opérationnelle/Projet/OptimisationTourneeDrones/exemple.dat")
 
+include("Projet_Base.jl")
 
-function creationDistancier(nom_fichier::String)
+function solve_CW(nom_fichier::String)
 
- data = lecture_donnees(nom_fichier)
- 
- distanceInitiale::Array{Int64,2} = data.distance
- 
- tailleMatrice::Int64 = data.nbVilles 
+	data = lecture_donnees(nom_fichier)
 
+	distanceInitiale::Array{Int64,2} = data.distance
+	tailleMatrice::Int64 = data.nbVilles
+	capacite::Int64 = data.capacite
+	listeDemande::Array{Int64} = data.demande
+
+	listeCouple::Array{Tuple{Int64,Int64}} = creationDistancier(distanceInitiale, tailleMatrice)
+
+	resultat = []
+
+	for i in 1:size(listeCouple,1)
+
+    	indice1 = findfirst(x-> listeCouple[i][1] in x, resultat) #Récupère l'indice de la tournée contenant le premier client du couple
+    	indice2 = findfirst(x-> listeCouple[i][2] in x, resultat) #Récupère l'indice de la tournée contenant le deuxième client du couple
+
+		
+    	if indice1 === nothing && indice2 === nothing && verifDemande([listeCouple[i][1]],[listeCouple[i][2]],listeDemande,capacite) #Condition si les 2 clients du couple i ne sont pas dans la liste des tournées
+        	push!(resultat, [listeCouple[i][1],listeCouple[i][2]])
+    	elseif indice1 !== nothing && indice2 === nothing && verifDemande(resultat[indice1],[listeCouple[i][2]],listeDemande,capacite) #Condition si le premier client du couple i est dans la liste des tournées mais pas le deuxième
+			if resultat[indice1][1] == listeCouple[i][1]
+				prepend!(resultat[indice1],listeCouple[i][2])
+			elseif resultat[indice1][size(resultat[indice1],1)] == listeCouple[i][1]
+				push!(resultat[indice1],listeCouple[i][2])
+			end
+		elseif indice1 === nothing && indice2 !== nothing && verifDemande(resultat[indice2],[listeCouple[i][1]],listeDemande,capacite) #Condition si le deuxième client du couple i est dans la liste des tournées mais pas le premier
+			if resultat[indice2][1] == listeCouple[i][2]
+				prepend!(resultat[indice2],listeCouple[i][1])
+			elseif resultat[indice2][size(resultat[indice2],1)] == listeCouple[i][2]
+				push!(resultat[indice2],listeCouple[i][1])
+			end
+		elseif indice1 !== nothing && indice2 !== nothing && indice1 != indice2 && verifDemande(resultat[indice1],resultat[indice2],listeDemande,capacite) #Condition si les 2 clients du couple i sont dans la liste des tournées
+			if resultat[indice1][1] == listeCouple[i][1] && resultat[indice2][1] == listeCouple[i][2]
+				reverse!(resultat[indice1])
+				for j in 1:size(resultat[indice2],1)
+					push!(resultat[indice1],resultat[indice2][j])
+				end
+				deleteat!(resultat,indice2)
+			elseif resultat[indice1][1] == listeCouple[i][1] && resultat[indice2][size(resultat[indice2],1)] == listeCouple[i][2]
+				for j in 1:size(resultat[indice1],1)
+					push!(resultat[indice2],resultat[indice1][j])
+				end
+				deleteat!(resultat,indice1)
+			elseif resultat[indice1][size(resultat[indice1],1)] == listeCouple[i][1] && resultat[indice2][1] == listeCouple[i][2]
+				for j in 1:size(resultat[indice2],1)
+					push!(resultat[indice1],resultat[indice2][j])
+				end
+				deleteat!(resultat,indice2)
+			elseif resultat[indice1][size(resultat[indice1],1)] == listeCouple[i][1] && resultat[indice2][size(resultat[indice2],1)] == listeCouple[i][2]
+				reverse!(resultat[indice1],1)
+				for j in 1:size(resultat[indice1])
+					push!(resultat[indice2],resultat[indice1][j])
+				end
+				deleteat!(resultat,indice1)
+			end
+		end
+	end
+
+	for i in 2:(tailleMatrice)
+		if findfirst(x-> i in x, resultat) === nothing
+			push!(resultat,[i])
+		end
+	end
+
+	cpt::Int64 = 1
+	for i in 1:size(resultat,1)
+		println("Tournée ",cpt,": ",resultat[i])
+		cpt += 1
+	end
+end
+
+function verifDemande(tab1::Array{Int64}, tab2::Array{Int64}, listeDemande::Array{Int64}, limite::Int64)
+	resultat::Int64 = 0
+	for i in 1:size(tab1,1)
+		resultat += listeDemande[tab1[i]-1]
+	end
+	for i in 1:size(tab2,1)
+		resultat += listeDemande[tab2[i]-1]
+	end
+
+	return resultat <= limite
+end
+
+
+function creationDistancier(distanceInitiale::Array{Int64,2}, tailleMatrice::Int64)
  
  #distancierOptimise::Array{Int64,2} = Array{Int64,2}(undef,tailleMatrice,tailleMatrice)
  distancierOptimise::Vector{Tuple{Int64,Tuple{Int64,Int64}}} = []
